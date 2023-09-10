3adf7da06edd43059219b68eab5558b829d46536
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-09-10 06:14:00 +0530

    commit on terminal

--- src/Advertisement.js
+++ src/Advertisement.js
@@ -4,7 +4,7 @@ function Advertisement() {
   return (
     <div className="advertisement">
       <hr />
-      Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .
+      Submitted and coded by Jagadeesh Kumar . S .
       <hr />
     </div>
   );
